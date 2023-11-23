7e26197cf24c558ef482f45b663f103dbc54398c
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-23 13:11:58 +0700

    Percobaan Praktikum 2.2

--- Terimakasih_07.java
+++ Terimakasih_07.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
 public class Terimakasih_07{
-    
-     public static void main(String[] args){
-        
+
+    public static void UcapanTerimakasih(){
         System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you abything.");
-
-
     }
+
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
+
+    public static void main(String[] args){
+        UcapanTerimakasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
+    }
+
 }
