e6705994620f16513ff2c73a54d317c28eafb0f7
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-24 03:26:06 +0700

    Percobaan Praktikum 6 menggunakan fungsi

--- Percobaan607.java
+++ Percobaan607.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class Percobaan607 {
 
+    static int hitungLuas (int pjg, int lb){
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume (int tinggi, int a, int b){
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+
     public static void main(String[] args){
-        
-        Scanner scan  = new Scanner(System.in);
-
-        int p,l,t,L,vol;
-
+        Scanner scan = new Scanner(System.in);
+        int p,l,t,L, vol;
         System.out.println("Masukan panjang");
         p = scan.nextInt();
         System.out.println("Masukan lebar");
@@ -15,11 +22,10 @@ public class Percobaan607 {
         System.out.println("Masukan tinggi");
         t = scan.nextInt();
 
-        L = p*l;
-        System.out.println("Luas persegi panjang adalah "+L);
-
-        vol = p*l*t;
-        System.out.println("Volume balok adalah "+vol);
+        L = hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah "+L);
+        vol = hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah "+vol);
     }
- 
+    
 }
