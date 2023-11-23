1fbb84278bb36a64d8a2db26831dc45d85a0c97d
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-23 10:34:08 +0700

    Modifkasi 2.1

--- Terimakasih_07.java
+++ Terimakasih_07.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Terimakasih_07{
-
-    public static void UcapanTerimakasih(){
+    
+     public static void main(String[] args){
+        
         System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you abything.");
-    }
 
-    public static void main(String[] args){
-        UcapanTerimakasih();
+
     }
 }
